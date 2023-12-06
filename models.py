b2fe8e386aed2b78bc19b8e9c5b1f019f3233df2
Jia Rong Gan <jiaronggan21@gmail.com>
2023-12-06 17:43:52 -0600

    added templates, app and class User on models.py

--- models.py
+++ models.py
@@ -1,1 +1,42 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """ User class that includes an id as primary key, first name, last name
+    and profile image url. """
+
+    __tablename__= "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    first_name = db.Column(
+        db.String(50),
+        nullable=False
+        )
+
+    last_name = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    image_url = db.Column(
+        db.String(300),
+    )
+
+
+
