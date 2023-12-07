38cb57ebe7e50909f64a2d6cb364a51d893f9e43
Matt Smida <mcs@m4t7.com>
2023-12-06 19:43:50 -0500

    added a user list homepage and form for adding new users, and the routes with view functions

--- app.py
+++ app.py
@@ -24,3 +24,28 @@ def list_users():
 
     users = User.query.all()
     return render_template("list.html", users=users)
+
+
+
+@app.get("/users/new")
+def show_new_user_form():
+    """ Show new user form. """
+    return render_template('new-user.html')
+
+@app.post("/users/new")
+def create_user():
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url'] if request.form['image_url'] else None
+
+    new_person = User(
+        first_name=first_name,
+        last_name=last_name,
+        image_url=image_url
+        )
+
+    db.session.add(new_person)
+    db.session.commit()
+
+    return redirect('/')
